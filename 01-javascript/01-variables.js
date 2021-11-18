c80482efd0005ee747f2990eb9cb014f7afdbd47
Peterca <pierre.dolce@epn.edu.ec>
2021-11-18 08:30:44 -0500

    update/variables terminadas y empezar arreglos

--- 01-javascript/01-variables.js
+++ 01-javascript/01-variables.js
@@ -46,51 +46,51 @@ console.log(Number("asd")) //nan
 }*/
 
 //Falsy
-if(""){
+if ("") {
     console.log("string vacio es es verdadero");
-    }else {
+} else {
     console.log("string vacio es es falsy");
-    }
+}
 
-    //Truty
-if("Pierre"){
+//Truty
+if ("Pierre") {
     console.log("string con dato es truty");
-}else {
+} else {
     console.log("string con dato es falsy");
 }
 
 //Negativo
-if(-1){
+if (-1) {
     console.log("Negativo es truty");
-}else {
+} else {
     console.log("Negativo es falsy");
 }
 
 //Cero
-if(0){
+if (0) {
     console.log("Cero es truty");
-}else {
+} else {
     console.log("Cero es falsy");
 }
 
 //Positivo
-if(1){
+if (1) {
     console.log("Positivo es truty");
-}else {
+} else {
     console.log("Positivo es falsy");
 }
 
 //Null
-if(null){
+if (null) {
     console.log("Null es truty");
-}else {
+} else {
     console.log("Null es falsy");
 }
 
 //Undefined
-if(undefined){
+if (undefined) {
     console.log("Undefined es truty");
-}else {
+} else {
     console.log("Undefined es falsy");
 }
 
@@ -115,3 +115,78 @@ const pierre = {
 
 console.log(pierre);
 
+//Acceder a las propiedades del objeto
+pierre.nombre; //"pierre"
+pierre.apellido; //"dolce"
+pierre["nombre"]; //"pierre"
+console.log(pierre);
+
+pierre.nombre = "Peter";
+console.log(pierre);
+pierre["nombre"] = "Peterca";
+console.log(pierre);
+pierre.sueldo; //undefined
+console.log(pierre.sueldo);
+
+//Agregar nuevas propiedades al objeto
+pierre.sueldo = 1.2;
+pierre["gastos"] = 0.8;
+console.log(pierre.gastos);
+console.log(pierre);
+console.log(Object.keys(pierre));
+console.log(Object.values(pierre));
+
+//Eliminar claves del objeto
+delete pierre.nombre; //Eliminar la clave nombre
+console.log(pierre);
+
+//Variables por valor y por referencia
+//Variables por valor en JS son las primitivas: number, string, boolean, null, undefined
+let edadPierre = 23;
+let edadDolce = edadPierre; //Guardamos una primitiva en otra variable
+                            //variables por valor
+console.log(edadPierre); //23
+console.log(edadDolce); //23
+edadPierre++;
+console.log(edadPierre); //24
+console.log(edadDolce); //23
+
+
+//Variables por referencia en JS son los objetos: object, array ({}, [])
+// let peter = {
+//     nombre: "peter",
+// }
+// let peterca = peter; //Guardamos un objeto en otra variable
+// //variables por referencia
+// console.log(peter);
+// console.log(peterca);
+// peterca.nombre = "peterca";
+// peterca.apellido = "dolce";
+// console.log(peter);
+// console.log(peterca);
+// delete peterca.nombre;
+// console.log(peter);
+// console.log(peterca);
+
+let peter = {
+    nombre: "peter",
+}
+let peterca = Object.assign({},peter);
+console.log(peter);
+console.log(peterca);
+peterca.nombre = "peterca";
+peterca.apellido = "dolce";
+console.log(peter);
+console.log(peterca);
+delete peterca.nombre;
+console.log(peter);
+console.log(peterca);
+
+let arregloNumeros = [1,2,3,4,5];
+let arregloClonado = Object.assign([],arregloNumeros);
+console.log(arregloNumeros);
+console.log(arregloClonado);
+arregloNumeros[0] = 200;
+arregloClonado[0] = 100;
+console.log(arregloNumeros);
+console.log(arregloClonado);
